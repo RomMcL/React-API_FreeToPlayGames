55a186dd88077b142fd15582eee14275d6cb6654
RomMcL <rommcl@yandex.ru>
2025-02-13 06:14:31 +0300

    Подключение к API
    CORS Support - RapidApi на https://rapidapi.com

--- src/components/pages/ListPage.jsx
+++ src/components/pages/ListPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { changeGamesList, changeSelectedGame } from '../../redux-state/redusers/data';
 
@@ -12,18 +12,56 @@ const ListPage = () => {
     const gamesList = useSelector(state => state.dataSlice.gamesList);
     const selectedGame = useSelector(state => state.dataSlice.selectedGame);
     const dispatch = useDispatch();
+
+
+
+    const BASE_URL = 'https://free-to-play-games-database.p.rapidapi.com/api';
+    const OPTIONS = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'f858958a6amshc313acb38be0fd6p12e43ejsn128ec01297e2',
+            'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        },
+    };
+
+    const fetchData = async (request) => {
+ 
+        try {            
+            const response = await fetch(`${BASE_URL}${request}`, OPTIONS);
+            if (!response.ok) throw new Error(response.status);                                                 
+            const data = await response.json();
+            dispatch(changeGamesList(data));
+        } catch (err) {
+
+            console.log(err.message);
+             
+        } finally {
+            console.log(`Запрос обработан`);
+        }
+    };
+
+
+    useEffect(() => {
+        fetchData('/games');
+    }, []);
+
+    console.log(gamesList)
+
     
     return (      
     <MainContainer>
-        <h1>List Page</h1>
 
         <div>
-            <h2 onClick={() => dispatch(changeGamesList('есть список'))}>Список</h2>
-            <h2 onClick={() => dispatch(changeSelectedGame('есть игра'))}>Игра</h2>
-            <p>Список: {gamesList}</p>
-            <p>Игра: {selectedGame}</p>
+            {
+                gamesList?.map(game => (
+                    <p key={game.id}>{game.title}</p>                                 
+                ))
+            }
+
         </div>
-        
+
+        <h1>List Page</h1>
+               
     </MainContainer>     
     )
 }
--- src/redux-state/redusers/data.js
+++ src/redux-state/redusers/data.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    gamesList: '',
-    selectedGame: '',
+    gamesList: [],
+    selectedGame: {},
 
 }
 
